56a5cc5da24475e72246316ef47a272b4e39007c
Carlos Garcia <gscarlos.enri@gmail.com>
2025-12-08 23:25:21 -0600

    Manipulacion con ORM

--- minilibrary/models.py
+++ minilibrary/models.py
@@ -5,6 +5,10 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     birth_date = models.DateField(null=True, blank=True)
     
+    def __str__(self):
+        return self.name
+    
+    
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_date = models.DateField(null=True, blank=True)
@@ -12,3 +16,6 @@ class Book(models.Model):
         Author, on_delete=models.CASCADE, related_name='books') #me permite acceder a los libros del author, con comando author.books__all
     pages = models.IntegerField()
     isbn = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.title
